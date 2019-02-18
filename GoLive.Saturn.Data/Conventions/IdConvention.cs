09a2f8494978aa88335041b41110fe26b150fefd
Monty <ma@surgicalcoder.com>
2019-02-18 19:57:59 +0000

    Initial Commit

---
+++ GoLive.Saturn.Data/Conventions/IdConvention.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace GoLive.Saturn.Data.Conventions
+{
+    public class IdConvention : ConventionBase, IClassMapConvention
+    {
+        public void Apply(BsonClassMap classMap)
+        {
+            var enumerable = classMap.AllMemberMaps.Select(x => x.ElementName).ToList();
+
+            var items = enumerable.Where(x => enumerable.Contains(x.ToLower() + "id"));
+
+            foreach (var val in items)
+            {
+                classMap.UnmapProperty(val);
+            }
+        }
+    }
+}
---
+++ GoLive.Saturn.Data/Repository.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using GoLive.Saturn.Data.Abstractions;
+using GoLive.Saturn.Data.Conventions;
+using GoLive.Saturn.Data.Entities;
+using GoLive.Saturn.Data.EntitySerializers;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
+using Newtonsoft.Json.Linq;
+
+namespace GoLive.Saturn.Data
+{
+    public class Repository : IRepository, IDisposable
+    {
+        #region Props
+        private static RepositoryOptions options { get; set; }
+
+        public static bool InitRun { get; set; }
+        public static DateTime InitLastChecked { get; set; }
+
+        private IMongoDatabase mongoDatabase { get; set; }
+        private MongoClient client { get; set; }
+
+        #endregion
+
+        public Repository(IConfiguration configuration, RepositoryOptions repositoryOptions = null)
+        {
+            options = repositoryOptions;
+
+            string connectionStringName = options?.ConnectionStringName ?? "ConnectionString";
+
+            var settings = MongoClientSettings.FromConnectionString(configuration[connectionStringName]);
+
+            if (configuration["Debug:OutputMongoDB"].ToLower() == "true" && options?.CommandStartedCallback != null)
+            {
+                settings.ClusterConfigurator = cb =>
+                {
+                    cb.Subscribe<CommandStartedEvent>(e =>
+                    {
+                        options?.CommandStartedCallback?.Invoke(e.CommandName, e.Command.ToJson());
+                    });
+                };
+            }
+
+            client = new MongoClient(settings);
+
+            var mongoUrl = new MongoUrl(configuration[connectionStringName]);
+            mongoDatabase = client.GetDatabase(mongoUrl.DatabaseName);
+
+            RegisterConventions();
+            InitDatabase();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose(bool val)
+        {
+            if (val) { }
+        }
+
+        public async Task Delete<T>(string Id, string overrideCollectionName = "") where T : Entity
+        {
+            await Delete<T>(f => f.Id == Id);
+        }
+
+        public async Task DeleteMany<T>(List<string> IDs, string overrideCollectionName = "") where T : Entity
+        {
+            if (!IDs.Any())
+            {
+                return;
+            }
+
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            await collection.DeleteManyAsync(f => IDs.Contains(f.Id));
+
+        }
+
+        public async Task DeleteMany<T>(IEnumerable<T> entity, string overrideCollectionName = "") where T : Entity
+        {
+            if (!entity.Any())
+            {
+                return;
+            }
+            var collection = GetCollection<T>(overrideCollectionName);
+            var list = entity.Select(r => r.Id).ToList();
+            await collection.DeleteManyAsync(arg => list.Contains(arg.Id));
+        }
+
+        public void DisposeConnection()
+        {
+            Dispose();
+        }
+
+        private void RegisterConventions()
+        {
+            var pack = new ConventionPack
+            {
+                new IgnoreIfDefaultConvention(true),
+                new IgnoreEmptyArraysConvention(),
+                new IgnoreExtraElementsConvention(true),
+                new NamedIdMemberConvention("Id"),
+                new StringObjectIdIdGeneratorConvention()
+            };
+
+            ConventionRegistry.Register("Custom Conventions", pack, t => true);
+
+            try
+            {
+                BsonSerializer.RegisterDiscriminatorConvention(typeof(JObject), new JObjectDiscriminatorConvention());
+
+                BsonSerializer.RegisterGenericSerializerDefinition(typeof(Ref<>), typeof(RefSerializer<>));
+                BsonSerializer.RegisterGenericSerializerDefinition(typeof(WeakRef<>), typeof(WeakRefSerializer<>));
+
+                BsonSerializer.RegisterSerializer(typeof(EncryptedString), new EncryptedStringSerializer());
+                BsonSerializer.RegisterSerializer(typeof(HashedString), new HashedStringSerializer());
+                BsonSerializer.RegisterSerializer(typeof(JObject), new JObjectSerializer());
+
+                BsonSerializer.RegisterSerializer(typeof(Timestamp), new TimestampSerializer());
+            }
+            catch (BsonSerializationException bsex) when (bsex.Message == "There is already a discriminator convention registered for type Newtonsoft.Json.Linq.JObject.") { }
+
+            if (options?.DiscriminatorConventions != null)
+            {
+                foreach (var convention in options?.DiscriminatorConventions)
+                {
+                    BsonSerializer.RegisterDiscriminatorConvention(convention.Key, convention.Value as IDiscriminatorConvention);
+                }
+
+            }
+
+            if (options?.GenericSerializers != null)
+            {
+                foreach (var serializer in options?.GenericSerializers)
+                {
+                    BsonSerializer.RegisterGenericSerializerDefinition(serializer.Key, serializer.Value);
+                }
+
+            }
+
+            if (options?.Serializers != null)
+            {
+                foreach (var serializer in options?.Serializers)
+                {
+                    BsonSerializer.RegisterSerializer(serializer.Key, serializer.Value as IBsonSerializer);
+                }
+            }
+
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Entity)))
+            {
+                BsonClassMap.RegisterClassMap<Entity>(delegate (BsonClassMap<Entity> map)
+                {
+                    map.AutoMap();
+
+                    map.MapProperty(f => f.Version).SetElementName("_v");
+
+                    map.IdMemberMap.SetSerializer(new StringSerializer().WithRepresentation(BsonType.ObjectId))
+                        .SetIdGenerator(StringObjectIdGenerator.Instance)
+                        .SetDefaultValue(ObjectId.GenerateNewId().ToString());
+                });
+            }
+        }
+
+        public void InitDatabase()
+        {
+            PopulateDatabase();
+        }
+
+        private void PopulateDatabase()
+        {
+            if (options?.InitDuration == null)
+            {
+                return;
+            }
+            if (InitRun && InitLastChecked > DateTime.Now.Add(options.InitDuration))
+            {
+                return;
+            }
+
+            InitRun = true;
+            InitLastChecked = DateTime.Now;
+            options?.InitCallback?.Invoke(this);
+        }
+
+        private string GetCollectionNameForType<T>(string collectionNameOverride)
+        {
+            if (options?.CollectionNameOverride != null)
+            {
+                return options?.CollectionNameOverride?.Invoke(collectionNameOverride);
+            }
+
+            return string.IsNullOrWhiteSpace(collectionNameOverride) ? typeof(T).Name : collectionNameOverride.Replace(".", "");
+        }
+
+
+        #region Get
+
+        public async Task<T> ById<T>(string id, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            var result = await collection.FindAsync(e => e.Id == id, new FindOptions<T> { Limit = 1 });
+
+            return await result.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> ById<T>(List<string> IDs, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+            var result = await collection.FindAsync(e => IDs.Contains(e.Id));
+            return await result.ToListAsync();
+        }
+
+        public async Task<List<Ref<T>>> ByRef<T>(List<Ref<T>> Item, string overrideCollectionName = "") where T : Entity
+        {
+            var enumerable = Item.Where(e => e != null).Select(f => f.Id).ToList();
+            var res = await ById<T>(enumerable, overrideCollectionName);
+
+            return res.Select(r => new Ref<T>(r)).ToList();
+        }
+
+        public async Task<T> ByRef<T>(Ref<T> Item, string overrideCollectionName = "") where T : Entity
+        {
+            return Item == null ? null : await ById<T>(Item.Id, overrideCollectionName);
+        }
+
+        public async Task<Ref<T>> PopulateRef<T>(Ref<T> Item, string overrideCollectionName = "") where T : Entity
+        {
+            if (Item == null || string.IsNullOrWhiteSpace(Item.Id))
+            {
+                return null;
+            }
+
+            Item.Item = await ById<T>(Item.Id, overrideCollectionName);
+
+            return Item;
+        }
+
+        public async Task<T> One<T>(Expression<Func<T, bool>> predicate, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            var result = await collection.FindAsync(predicate, new FindOptions<T> { Limit = 1 });
+
+            return await result.FirstOrDefaultAsync();
+        }
+
+
+        public async Task<List<T>> Many<T>(Dictionary<string, object> WhereClause, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = mongoDatabase.GetCollection<BsonDocument>(GetCollectionNameForType<T>(overrideCollectionName));
+            var where = new BsonDocument(WhereClause);
+            var result = await (await collection.FindAsync(where, null)).ToListAsync();
+
+            return result.Select(f => BsonSerializer.Deserialize<T>(f)).ToList();
+        }
+
+        private IMongoCollection<T> GetCollection<T>(string overrideCollectionName) where T : Entity
+        {
+            return mongoDatabase.GetCollection<T>(GetCollectionNameForType<T>(overrideCollectionName));
+        }
+
+        public async Task<long> CountMany<T>(Expression<Func<T, bool>> predicate, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+            return await collection.CountDocumentsAsync(predicate);
+        }
+
+
+        public async Task<IQueryable<T>> Many<T>(Expression<Func<T, bool>> predicate, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+            return await Task.Run(() => collection.AsQueryable().Where(predicate));
+        }
+
+        public async Task<IQueryable<T>> All<T>(string overrideCollectionName = "") where T : Entity
+        {
+            return await Task.Run(() => GetCollection<T>(overrideCollectionName).AsQueryable());
+        }
+
+        #endregion
+
+        #region Add
+
+        public async Task Add<T>(T entity, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            await collection.InsertOneAsync(entity);
+        }
+
+        public async Task AddMany<T>(IEnumerable<T> entities, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+            await collection.InsertManyAsync(entities, new InsertManyOptions() { IsOrdered = true });
+        }
+
+        #endregion
+
+        #region Delete
+
+        public async Task UpsertMany<T>(List<T> entity, string overrideCollectionName = "") where T : Entity
+        {
+            if (!entity.Any())
+            {
+                return;
+            }
+
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            var writeModel = entity.Select(f => new ReplaceOneModel<T>(new ExpressionFilterDefinition<T>(e => e.Id == f.Id), f) { IsUpsert = true });
+
+            var bulkWriteResult = await collection.BulkWriteAsync(writeModel);
+
+            if (!bulkWriteResult.IsAcknowledged)
+            {
+                throw new FailedToUpsertException();
+            }
+        }
+
+        public async Task Delete<T>(T entity, string overrideCollectionName = "") where T : Entity
+        {
+            await GetCollection<T>(overrideCollectionName).DeleteOneAsync(f => f.Id == entity.Id);
+        }
+
+        public async Task Delete<T>(Expression<Func<T, bool>> filter, string overrideCollectionName = "") where T : Entity
+        {
+            await GetCollection<T>(overrideCollectionName).DeleteManyAsync(filter);
+        }
+
+
+        #endregion
+
+        #region Update
+
+        public async Task UpdateMany<T>(List<T> entity, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            var writeModel = entity.Select(f => new ReplaceOneModel<T>(new ExpressionFilterDefinition<T>(e => e.Id == f.Id), f) { IsUpsert = false });
+
+            var bulkWriteResult = await collection.BulkWriteAsync(writeModel);
+
+            if (!bulkWriteResult.IsAcknowledged)
+            {
+                throw new FailedToUpdateException();
+            }
+        }
+
+        public async Task Upsert<T>(T entity, string overrideCollectionName = "") where T : Entity
+        {
+            if (string.IsNullOrWhiteSpace(entity.Id))
+            {
+                entity.Id = ObjectId.GenerateNewId().ToString();
+            }
+
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            var updateResult = await collection.ReplaceOneAsync(e => e.Id == entity.Id, entity, new UpdateOptions { IsUpsert = true });
+
+            if (!updateResult.IsAcknowledged)
+            {
+                throw new FailedToUpsertException();
+            }
+        }
+
+        public async Task<bool> Update<T>(T entity, string overrideCollectionName = "") where T : Entity
+        {
+            var collection = GetCollection<T>(overrideCollectionName);
+
+            var updateResult = await collection.ReplaceOneAsync(e => e.Id == entity.Id, entity, new UpdateOptions() { IsUpsert = false });
+
+            if (!updateResult.IsAcknowledged)
+            {
+                throw new FailedToUpdateException();
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+}
