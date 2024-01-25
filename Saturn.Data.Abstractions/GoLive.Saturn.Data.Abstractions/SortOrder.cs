a0315d0ec415d4d4d3df1242f8be27950ea7ab3b
SurgicalCoder <me@surgicalcoder.com>
2024-01-25 23:31:38 +0000

    Changing SortOrder signature
    Breaking change

--- Saturn.Data.Abstractions/GoLive.Saturn.Data.Abstractions/SortOrder.cs
+++ Saturn.Data.Abstractions/GoLive.Saturn.Data.Abstractions/SortOrder.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Linq.Expressions;
 using GoLive.Saturn.Data.Entities;
 
 namespace GoLive.Saturn.Data.Abstractions;
 
 public class SortOrder<T> where T :Entity
 {
+    public SortOrder() { }
+    public SortOrder(Expression<Func<T, object>> field, SortDirection direction)
+    {
+        Direction = direction;
+        Field = field;
+    }
     public SortDirection Direction { get; set; }
-    public Func<T, object> Field { get; set; }
+    public Expression<Func<T, object>> Field { get; set; }
 }
