31ac47061d5bf1b8158913772885e96681b6f4b0
SurgicalCoder <me@surgicalcoder.com>
2024-04-20 21:27:13 +0100

    Second Scope

---
+++ Saturn.Data.Abstractions/GoLive.Saturn.Data.Abstractions/ISecondScopedRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using GoLive.Saturn.Data.Entities;
+
+namespace GoLive.Saturn.Data.Abstractions;
+
+public interface ISecondScopedRepository 
+{
+    Task<TItem> ById<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, string id)
+        where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+  
+
+    Task<IQueryable<TItem>> All<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope)
+        where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+    
+
+    Task<TItem> One<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, Expression<Func<TItem, bool>> predicate, IEnumerable<SortOrder<TItem>> sortOrders = null)
+        where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+
+    Task<IQueryable<TItem>> Many<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, Expression<Func<TItem, bool>> predicate, 
+        int pageSize = 20, int pageNumber = 1, IEnumerable<SortOrder<TItem>> sortOrders = null)
+        where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+
+    Task<long> CountMany<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, Expression<Func<TItem, bool>> predicate)
+        where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+    
+    Task Insert<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, TItem entity)  where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+    
+    Task Update<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, TItem entity)  where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+    
+    Task Upsert<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, TItem entity)  where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+    
+    Task Delete<TItem, TSecondScope, TPrimaryScope>(Ref<TPrimaryScope> primaryScope, Ref<TSecondScope> secondScope, string Id)  where TItem : SecondScopedEntity<TSecondScope, TPrimaryScope>, new()
+        where TSecondScope : Entity, new() 
+        where TPrimaryScope : Entity, new();
+}
