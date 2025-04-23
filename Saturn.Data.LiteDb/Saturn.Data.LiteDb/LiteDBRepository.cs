370f800ab89ef84691e4d8b8d6c49414154ada27
SurgicalCoder <me@surgicalcoder.com>
2025-04-23 01:08:16 +0100

    Secondary fix for Ref<T>

--- Saturn.Data.LiteDb/Saturn.Data.LiteDb/LiteDBRepository.cs
+++ Saturn.Data.LiteDb/Saturn.Data.LiteDb/LiteDBRepository.cs
@@ -17,8 +17,58 @@ public partial class LiteDBRepository : IRepository
     public LiteDBRepository(RepositoryOptions repositoryOptions, LiteDBRepositoryOptions liteDbRepositoryOptions )
     {
         this.liteDbOptions = liteDbRepositoryOptions;
-        database = new LiteDatabaseAsync(repositoryOptions.ConnectionString, liteDbRepositoryOptions.Mapper);
+        var mapper = liteDbRepositoryOptions.Mapper;
+        
+        RegisterAllEntityRefs(mapper);
+        
+        database = new LiteDatabaseAsync(repositoryOptions.ConnectionString, mapper);
         options = repositoryOptions;
+    }
+    
+    
+    
+    protected void RegisterAllEntityRefs(BsonMapper mapper)
+    {
+        var entityBase = typeof(Entity);
+        var openRef    = typeof(Ref<>);
+
+        // 1) find every non-abstract Entity subclass with a public parameterless ctor
+        var entityTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => {
+                try { return a.GetTypes(); }
+                catch { return Array.Empty<Type>(); }
+            })
+            .Where(t =>
+                entityBase.IsAssignableFrom(t) &&
+                !t.IsAbstract &&
+                t.GetConstructor(Type.EmptyTypes) != null
+            );
+
+        foreach (var entityType in entityTypes)
+        {
+            // 2) construct the Ref<ThatEntity> type
+            var refType = openRef.MakeGenericType(entityType);
+            var idProp  = refType.GetProperty("Id")!;
+
+            // 3) serializer: take your Ref<ThatEntity>.Id → raw BsonValue
+            Func<object, BsonValue> serialize = obj =>
+            {
+                var idVal = idProp.GetValue(obj);  
+                return BsonMapper.Global.Serialize(idVal.GetType(), idVal);
+            };
+
+            // 4) deserializer: raw BsonValue → new Ref<ThatEntity> { Id = … }
+            Func<BsonValue, object> deserialize = bson =>
+            {
+                var inst  = Activator.CreateInstance(refType)!;
+                var clrId = BsonMapper.Global.Deserialize(idProp.PropertyType, bson);
+                idProp.SetValue(inst, clrId);
+                return inst;
+            };
+
+            // 5) register it
+            mapper.RegisterType(refType, serialize, deserialize);
+        }
     }
     
     public void Dispose()
