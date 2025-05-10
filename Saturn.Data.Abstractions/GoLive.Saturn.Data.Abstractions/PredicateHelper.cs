f6aee0d4f943dafc5fadfc2f825bfad751a399bb
SurgicalCoder <me@surgicalcoder.com>
2025-05-10 23:55:29 +0100

    Performance improvement

--- Saturn.Data.Abstractions/GoLive.Saturn.Data.Abstractions/PredicateHelper.cs
+++ Saturn.Data.Abstractions/GoLive.Saturn.Data.Abstractions/PredicateHelper.cs
@@ -8,18 +8,34 @@ public static class PredicateHelper
 {
     public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> exp, Expression<Func<T, bool>> newExp)
     {
-        var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), exp.Parameters.First());
-        newExp = visitor.Visit(newExp) as Expression<Func<T, bool>>;
-        var binExp = Expression.And(exp.Body, newExp.Body);
-        return Expression.Lambda<Func<T, bool>>(binExp, newExp.Parameters);
-    }        
-        
+        // Ensure both expressions have the same parameter type
+        if (exp.Parameters.Count != newExp.Parameters.Count)
+            throw new ArgumentException("Expressions must have the same number of parameters.");
+
+        // Reuse the parameter from the first expression
+        var parameter = exp.Parameters.First();
+        var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), parameter);
+        var newBody = visitor.Visit(newExp.Body);
+
+        // Combine the expressions directly
+        var binExp = Expression.And(exp.Body, newBody);
+        return Expression.Lambda<Func<T, bool>>(binExp, parameter);
+    }
+
     public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> exp, Expression<Func<T, bool>> newExp)
     {
-        var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), exp.Parameters.First());
-        newExp = visitor.Visit(newExp) as Expression<Func<T, bool>>;
-        var binExp = Expression.Or(exp.Body, newExp.Body);
-        return Expression.Lambda<Func<T, bool>>(binExp, newExp.Parameters);
+        // Ensure both expressions have the same parameter type
+        if (exp.Parameters.Count != newExp.Parameters.Count)
+            throw new ArgumentException("Expressions must have the same number of parameters.");
+
+        // Reuse the parameter from the first expression
+        var parameter = exp.Parameters.First();
+        var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), parameter);
+        var newBody = visitor.Visit(newExp.Body);
+
+        // Combine the expressions directly
+        var binExp = Expression.Or(exp.Body, newBody);
+        return Expression.Lambda<Func<T, bool>>(binExp, parameter);
     }
 
     public class ParameterUpdateVisitor : System.Linq.Expressions.ExpressionVisitor
---
+++ Saturn.Data/GoLive.Saturn.Data/ServicesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoLive.Saturn.Data.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceScan.SourceGenerator;
+
+namespace GoLive.Saturn.Data;
+
+public static partial class ServicesExtensions
+{
+    [GenerateServiceRegistrations(
+        TypeNameFilter = "*Repository",   
+        AsImplementedInterfaces = true,
+        Lifetime = ServiceLifetime.Singleton)]
+    public static partial IServiceCollection AddSaturnDataRepositoryServices(this IServiceCollection services);
+}
