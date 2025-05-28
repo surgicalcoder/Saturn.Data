75d3fa475a6549d62da417c2a7b77c6307f148c7
SurgicalCoder <me@surgicalcoder.com>
2025-05-28 23:22:43 +0100

    As Self actually needed

--- Saturn.Data.LiteDb/Saturn.Data.LiteDb/ServicesExtensions.cs
+++ Saturn.Data.LiteDb/Saturn.Data.LiteDb/ServicesExtensions.cs
@@ -8,6 +8,7 @@ public static partial class ServicesExtensions
     [GenerateServiceRegistrations(
         TypeNameFilter = "*Repository",
         AsImplementedInterfaces = true,
+        AsSelf = true,
         Lifetime = ServiceLifetime.Singleton)]
     public static partial IServiceCollection AddSaturnLiteDBRepositoryServices(this IServiceCollection services);
 }
