cd765b960785a64fdbad219fe4d32099f2ca9bed
SurgicalCoder <me@surgicalcoder.com>
2025-09-14 14:00:24 +0100

    Watch

---
+++ Saturn.Data.MongoDb/Saturn.Data.MongoDb/MongoDbRepository.Watch.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+using GoLive.Saturn.Data.Abstractions;
+using GoLive.Saturn.Data.Entities;
+using MongoDB.Driver;
+
+namespace Saturn.Data.MongoDb;
+
+public partial class MongoDbRepository
+{
+    public async Task Watch<TItem>(Expression<Func<ChangedEntity<TItem>, bool>> predicate, ChangeOperation operationFilter, Action<TItem, string, ChangeOperation> callback, IDatabaseTransaction transaction = null, CancellationToken cancellationToken = default) where TItem : Entity
+    {
+        var pipelineDefinition = new EmptyPipelineDefinition<ChangeStreamDocument<TItem>>();
+
+        var expression = Converter<ChangeStreamDocument<TItem>>.Convert(predicate);
+
+        var opType = (ChangeStreamOperationType)operationFilter;
+
+        var definition = pipelineDefinition.Match(expression).Match(e => e.OperationType == opType);
+
+        if (transaction != null)
+        {
+            await GetCollection<TItem>().WatchAsync(((MongoDbTransactionWrapper)transaction).Session, definition, cancellationToken: cancellationToken);
+        }
+        else
+        {
+            await GetCollection<TItem>().WatchAsync(definition, cancellationToken: cancellationToken);
+        }
+
+        var collection = GetCollection<TItem>();
+
+        using (var asyncCursor = await collection.WatchAsync(pipelineDefinition, cancellationToken: cancellationToken))
+        {
+            foreach (var changeStreamDocument in asyncCursor.ToEnumerable())
+            {
+                callback.Invoke(changeStreamDocument.FullDocument, changeStreamDocument?.DocumentKey[0]?.AsObjectId.ToString(), (ChangeOperation)changeStreamDocument.OperationType);
+            }
+        }
+    }
+}
