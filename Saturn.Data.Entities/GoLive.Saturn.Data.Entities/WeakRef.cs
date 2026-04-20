24f5cea0c7af6fbfd10aec01db51064fbcba61a1
SurgicalCoder <me@surgicalcoder.com>
2026-04-20 21:35:17 +0100

    Introduce non-generic WeakRef base class
    Refactor WeakRef<T> to inherit from a new non-generic WeakRef base class. This change improves ID synchronization between the reference and the underlying entity, and allows for handling weak references in contexts where the specific entity type is not known.

--- Saturn.Data.Entities/GoLive.Saturn.Data.Entities/WeakRef.cs
+++ Saturn.Data.Entities/GoLive.Saturn.Data.Entities/WeakRef.cs
@@ -3,18 +3,140 @@ using System.Collections.Generic;
 
 namespace GoLive.Saturn.Data.Entities;
 
-public class WeakRef<T> : IComparable<WeakRef<T>> where T : Entity
+public class WeakRef : IComparable<WeakRef>
 {
-    public T Item { get; set; }
-    public string Id { get; set; }
-    public WeakRef(string Id)
+    private string _refId;
+    private Entity item;
+
+    public virtual Entity Item
     {
-        this.Id = Id;
+        get => item;
+        set => item = value;
     }
 
-    public WeakRef(T reference)
+    public virtual string Id
     {
-        Item = reference ?? throw new ArgumentNullException(nameof(reference));
+        get
+        {
+            if (Item != null && !string.IsNullOrWhiteSpace(Item.Id))
+            {
+                _refId = Item.Id;
+                return Item.Id;
+            }
+
+            return _refId;
+        }
+        set => _refId = value;
+    }
+
+    public WeakRef(string Id)
+    {
+        _refId = Id;
+    }
+
+    public WeakRef(Entity reference)
+    {
+        item = reference ?? throw new ArgumentNullException(nameof(reference));
+
+        if (!string.IsNullOrWhiteSpace(reference.Id))
+        {
+            _refId = reference.Id;
+        }
+    }
+
+    public static implicit operator WeakRef(Entity item)
+    {
+        return new WeakRef(item);
+    }
+
+    public static implicit operator WeakRef(string item)
+    {
+        return new WeakRef(item);
+    }
+
+    public int CompareTo(WeakRef other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        return string.Compare(Id, other.Id, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    protected bool Equals(WeakRef other)
+    {
+        return string.Equals(Id, other.Id, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj is WeakRef other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return (Id != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(Id) : 0);
+    }
+
+    public static bool operator ==(WeakRef left, WeakRef right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator ==(WeakRef left, string right)
+    {
+        if (left is null || String.IsNullOrWhiteSpace(right) || string.IsNullOrWhiteSpace(left.Id))
+        {
+            return false;
+        }
+        return left.Id.Equals(right, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static bool operator !=(WeakRef left, string right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator !=(WeakRef left, WeakRef right)
+    {
+        return !Equals(left, right);
+    }
+
+    public static bool operator <(WeakRef left, WeakRef right)
+    {
+        return Comparer<WeakRef>.Default.Compare(left, right) < 0;
+    }
+
+    public static bool operator >(WeakRef left, WeakRef right)
+    {
+        return Comparer<WeakRef>.Default.Compare(left, right) > 0;
+    }
+
+    public static bool operator <=(WeakRef left, WeakRef right)
+    {
+        return Comparer<WeakRef>.Default.Compare(left, right) <= 0;
+    }
+
+    public static bool operator >=(WeakRef left, WeakRef right)
+    {
+        return Comparer<WeakRef>.Default.Compare(left, right) >= 0;
+    }
+}
+
+public class WeakRef<T> : WeakRef, IComparable<WeakRef<T>> where T : Entity
+{
+    public new T Item
+    {
+        get => (T)base.Item;
+        set => base.Item = value;
+    }
+
+    public WeakRef(string Id) : base(Id)
+    {
+    }
+
+    public WeakRef(T reference) : base(reference)
+    {
     }
 
     public static implicit operator WeakRef<T>(T item)
@@ -29,27 +151,24 @@ public class WeakRef<T> : IComparable<WeakRef<T>> where T : Entity
 
     public int CompareTo(WeakRef<T> other)
     {
-        if (ReferenceEquals(this, other)) return 0;
-        if (ReferenceEquals(null, other)) return 1;
-        return string.Compare(Id, other.Id, StringComparison.InvariantCultureIgnoreCase);
+        return base.CompareTo(other);
     }
 
     protected bool Equals(WeakRef<T> other)
     {
-        return string.Equals(Id, other.Id, StringComparison.InvariantCultureIgnoreCase);
+        return other is not null && string.Equals(Id, other.Id, StringComparison.InvariantCultureIgnoreCase);
     }
 
     public override bool Equals(object obj)
     {
         if (ReferenceEquals(null, obj)) return false;
         if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((WeakRef<T>)obj);
+        return obj is WeakRef other && string.Equals(Id, other.Id, StringComparison.InvariantCultureIgnoreCase);
     }
 
     public override int GetHashCode()
     {
-        return (Id != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(Id) : 0);
+        return base.GetHashCode();
     }
 
     public static bool operator ==(WeakRef<T> left, WeakRef<T> right)
