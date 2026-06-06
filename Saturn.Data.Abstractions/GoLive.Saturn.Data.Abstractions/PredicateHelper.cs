480d8fcb0fea5fa4f1d1728fd4d9113e7e438044
SurgicalCoder <me@surgicalcoder.com>
2026-06-06 19:30:46 +0100

    Extension methods.

--- Saturn.Data.Abstractions/GoLive.Saturn.Data.Abstractions/PredicateHelper.cs
+++ Saturn.Data.Abstractions/GoLive.Saturn.Data.Abstractions/PredicateHelper.cs
@@ -6,36 +6,39 @@ namespace GoLive.Saturn.Data.Abstractions;
 
 public static class PredicateHelper
 {
-    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> exp, Expression<Func<T, bool>> newExp)
+    extension<T>(Expression<Func<T, bool>> exp)
     {
-        // Ensure both expressions have the same parameter type
-        if (exp.Parameters.Count != newExp.Parameters.Count)
-            throw new ArgumentException("Expressions must have the same number of parameters.");
+        public Expression<Func<T, bool>> And(Expression<Func<T, bool>> newExp)
+        {
+            // Ensure both expressions have the same parameter type
+            if (exp.Parameters.Count != newExp.Parameters.Count)
+                throw new ArgumentException("Expressions must have the same number of parameters.");
 
-        // Reuse the parameter from the first expression
-        var parameter = exp.Parameters.First();
-        var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), parameter);
-        var newBody = visitor.Visit(newExp.Body);
+            // Reuse the parameter from the first expression
+            var parameter = exp.Parameters.First();
+            var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), parameter);
+            var newBody = visitor.Visit(newExp.Body);
 
-        // Combine the expressions directly
-        var binExp = Expression.And(exp.Body, newBody);
-        return Expression.Lambda<Func<T, bool>>(binExp, parameter);
-    }
+            // Combine the expressions directly
+            var binExp = Expression.And(exp.Body, newBody);
+            return Expression.Lambda<Func<T, bool>>(binExp, parameter);
+        }
 
-    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> exp, Expression<Func<T, bool>> newExp)
-    {
-        // Ensure both expressions have the same parameter type
-        if (exp.Parameters.Count != newExp.Parameters.Count)
-            throw new ArgumentException("Expressions must have the same number of parameters.");
+        public Expression<Func<T, bool>> Or(Expression<Func<T, bool>> newExp)
+        {
+            // Ensure both expressions have the same parameter type
+            if (exp.Parameters.Count != newExp.Parameters.Count)
+                throw new ArgumentException("Expressions must have the same number of parameters.");
 
-        // Reuse the parameter from the first expression
-        var parameter = exp.Parameters.First();
-        var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), parameter);
-        var newBody = visitor.Visit(newExp.Body);
+            // Reuse the parameter from the first expression
+            var parameter = exp.Parameters.First();
+            var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), parameter);
+            var newBody = visitor.Visit(newExp.Body);
 
-        // Combine the expressions directly
-        var binExp = Expression.Or(exp.Body, newBody);
-        return Expression.Lambda<Func<T, bool>>(binExp, parameter);
+            // Combine the expressions directly
+            var binExp = Expression.Or(exp.Body, newBody);
+            return Expression.Lambda<Func<T, bool>>(binExp, parameter);
+        }
     }
 
     public class ParameterUpdateVisitor : System.Linq.Expressions.ExpressionVisitor
